8b186ab63a906e8ac2f989d4bb0e365a18f55fd2
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-07 05:53:37 +0300

    Edit background colors

--- lib/core/widgets/gradient_background.dart
+++ lib/core/widgets/gradient_background.dart
@@ -2,84 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tax_payer/core/utils/app_colors.dart';
 import 'package:tax_payer/core/utils/app_images.dart';
+import 'package:tax_payer/core/widgets/blur_circle.dart';
 
 class GradientBackground extends StatelessWidget {
-  const GradientBackground({super.key, required this.child, this.hasImage});
+  const GradientBackground({
+    super.key,
+    required this.child,
+    this.hasImage,
+    this.backgroundColors,
+  });
   final Widget child;
   final bool? hasImage;
+  final Widget? backgroundColors;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned(
-          top: -44,
-          left: -233,
-          child: _BlurCircle(
-            color: Colors.purpleAccent.withOpacity(0.35),
-            size: 350,
-          ),
-        ),
-        Positioned(
-          top: 120,
-          left: 233,
-          child: _BlurCircle(
-            color: AppColors.primaryColor(context).withOpacity(0.35),
-            size: 350,
-          ),
-        ),
-        Visibility(
-          visible: hasImage ?? false,
-          child: Positioned(
-            bottom: 0,
-            right: 0,
-            top: -530,
-            child: SvgPicture.asset(Assets.assetsImagesBackImage),
-          ),
-        ),
-        child,
-      ],
-    );
+    return backgroundColors != null
+        ? backgroundColors!
+        : Stack(
+          children: [
+            Positioned(
+              top: -44,
+              left: -233,
+              child: BlurCircle(
+                color: Colors.purpleAccent.withOpacity(0.35),
+                size: 350,
+              ),
+            ),
+            Positioned(
+              top: 120,
+              left: 233,
+              child: BlurCircle(
+                color: AppColors.primaryColor(context).withOpacity(0.35),
+                size: 350,
+              ),
+            ),
+            Visibility(
+              visible: hasImage ?? false,
+              child: Positioned(
+                bottom: 0,
+                right: 0,
+                top: -530,
+                child: SvgPicture.asset(Assets.assetsImagesBackImage),
+              ),
+            ),
+            child,
+          ],
+        );
   }
-}
-
-class _BlurCircle extends StatelessWidget {
-  final Color color;
-  final double size;
-
-  const _BlurCircle({required this.color, required this.size});
-
-  @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      size: Size(size, size),
-      painter: _BlurCirclePainter(color: color, size: size),
-    );
-  }
-}
-
-class _BlurCirclePainter extends CustomPainter {
-  final Color color;
-  final double size;
-
-  _BlurCirclePainter({required this.color, required this.size});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint =
-        Paint()
-          ..color = color
-          ..maskFilter = const MaskFilter.blur(
-            BlurStyle.normal,
-            50,
-          ); // Blur effect
-
-    canvas.drawCircle(
-      Offset(size.width / 2, size.height / 2),
-      size.width / 2,
-      paint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
