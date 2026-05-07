9bfa1f697bb80c423f3ef3e85f5f85f3e5efa581
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-07 05:59:14 +0300

    Remove unnecessary padding from OnBoardingPage widget

--- lib/Features/OnBoarding/presentation/views/widgets/on_boarding_page.dart
+++ lib/Features/OnBoarding/presentation/views/widgets/on_boarding_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:tax_payer/Features/OnBoarding/data/models/on_boarding_model.dart';
-import 'package:tax_payer/core/constants/app_spacing.dart';
 
 class OnBoardingPage extends StatelessWidget {
   const OnBoardingPage({super.key, required this.page});
@@ -9,9 +8,6 @@ class OnBoardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: AppSpacing.s24),
-      child: RepaintBoundary(child: Center(child: Image.asset(page.image))),
-    );
+    return RepaintBoundary(child: Center(child: Image.asset(page.image)));
   }
 }
