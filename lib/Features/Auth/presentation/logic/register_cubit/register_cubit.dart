1648db2ba81ebdbcf2d2039c50471c16f47228cc
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-07 06:02:48 +0300

    Adding Registe Cubit and state

---
+++ lib/Features/Auth/presentation/logic/register_cubit/register_cubit.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tax_payer/Features/Auth/data/repos/auth_repo.dart';
+import 'package:tax_payer/Features/Auth/presentation/logic/register_cubit/register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  final AuthRepo authRepo;
+  RegisterCubit(this.authRepo) : super(RegisterInitial());
+
+  Future<void> register({
+    required String firstName,
+    required String lastName,
+    required String phone,
+    required String idCard,
+    required File image,
+    required String userName,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    emit(RegisterLoading());
+    var result = await authRepo.register(
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      idCard: idCard,
+      image: image,
+      userName: userName,
+      password: password,
+      confirmPassword: confirmPassword,
+    );
+    result.fold(
+      (failure) => emit(RegisterFailure(errorMessage: failure.errorMessage)),
+      (message) => emit(RegisterSuccess(message: message)),
+    );
+  }
+}
