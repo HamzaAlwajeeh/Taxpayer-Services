ab4b07bb50cedb159b5c79a2b648a49627687db7
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-08 19:21:08 +0300

    Implement logout functionality in ProfileView and add localization support for Logout

--- lib/Features/Profile/presentation/views/widgets/profile_view_body.dart
+++ lib/Features/Profile/presentation/views/widgets/profile_view_body.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tax_payer/Features/Auth/presentation/logic/logout_cubit/logout_cubit.dart';
+import 'package:tax_payer/Features/Auth/presentation/logic/logout_cubit/logout_state.dart';
+import 'package:tax_payer/core/constants/constants.dart';
+import 'package:tax_payer/core/helper/custom_loading_indicator.dart';
+import 'package:tax_payer/core/services/shared_pref_singleton.dart';
+import 'package:tax_payer/core/widgets/custom_button.dart';
+import 'package:tax_payer/generated/l10n.dart';
 
 class ProfileViewBody extends StatelessWidget {
   const ProfileViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('مرحبًا بك في صفحة الملف الشخصي!'));
+    return Center(
+      child: BlocBuilder<LogoutCubit, LogoutState>(
+        builder:
+            (BuildContext context, state) =>
+                state is LogoutLoading
+                    ? CustomLoadingIndicator()
+                    : CustomButton(
+                      title: S.of(context).Logout,
+                      onPressed: () {
+                        BlocProvider.of<LogoutCubit>(context).logout();
+                        Prefs.setBool(AppConstants.kIsLogedIn, false);
+                      },
+                    ),
+      ),
+    );
   }
 }
