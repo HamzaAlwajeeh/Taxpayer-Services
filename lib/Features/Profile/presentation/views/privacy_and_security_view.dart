fd99e2319922a7c543cd7462b9671856ca2e362c
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-29 20:53:37 +0300

    feat: update Privacy and Security view with custom app bar and localization support

--- lib/Features/Profile/presentation/views/privacy_and_security_view.dart
+++ lib/Features/Profile/presentation/views/privacy_and_security_view.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:tax_payer/Features/Profile/presentation/views/widgets/privacy_and_security_view_body.dart';
-import 'package:tax_payer/core/widgets/gradient_background.dart';
+import 'package:tax_payer/core/utils/app_colors.dart';
+import 'package:tax_payer/core/widgets/custom_app_bar.dart';
+import 'package:tax_payer/generated/l10n.dart';
 
 class PrivacyAndSecurityView extends StatelessWidget {
   const PrivacyAndSecurityView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GradientBackground(child: PrivacyAndSecurityViewBody());
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: S.of(context).SecurityAndPrivacy,
+        color: AppColors.primaryColor(context),
+      ),
+      body: const PrivacyAndSecurityViewBody(),
+    );
   }
 }
