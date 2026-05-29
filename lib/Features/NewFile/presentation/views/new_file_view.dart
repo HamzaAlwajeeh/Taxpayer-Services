1f95558b212f8626b16e7cc805acebdf83dbc240
HamzaAlwajeeh <ha20mza60@gmail.com>
2026-05-29 20:59:42 +0300

    adding customAppBar

--- lib/Features/NewFile/presentation/views/new_file_view.dart
+++ lib/Features/NewFile/presentation/views/new_file_view.dart
@@ -4,7 +4,9 @@ import 'package:tax_payer/Features/NewFile/data/repos/new_file_repo.dart';
 import 'package:tax_payer/Features/NewFile/presentation/logic/new_file_cubit/new_file_cubit.dart';
 import 'package:tax_payer/Features/NewFile/presentation/views/widgets/new_file_view_body.dart';
 import 'package:tax_payer/core/services/service_locator.dart';
-import 'package:tax_payer/core/widgets/gradient_background.dart';
+import 'package:tax_payer/core/utils/app_colors.dart';
+import 'package:tax_payer/core/widgets/custom_app_bar.dart';
+import 'package:tax_payer/generated/l10n.dart';
 
 class NewFileView extends StatelessWidget {
   const NewFileView({super.key});
@@ -13,7 +15,13 @@ class NewFileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => NewFileCubit(getIt<NewFileRepo>()),
-      child: Scaffold(body: GradientBackground(child: const NewFileViewBody())),
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: S.of(context).NewFile,
+          color: AppColors.primaryColor(context),
+        ),
+        body: const NewFileViewBody(),
+      ),
     );
   }
 }
